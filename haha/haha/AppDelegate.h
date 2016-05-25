1896f1f713d2503b7768c48780ecb3b3056ad1dd
c ma <mac@yydemacbook-pro-4.local>
2016-05-25 14:44:26 +0800

    gangkaishi

---
+++ haha/haha/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  haha
+//
+//  Created by ma c on 16/5/25.
+//  Copyright © 2016年 YYN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ haha/haha/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  haha
+//
+//  Created by ma c on 16/5/25.
+//  Copyright © 2016年 YYN. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"----hkgsdjfhg----");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
